f4b35f3a5893eba7a9cb89f5d4dbc7368d7d149d
abhay gupta <akg568794@gmail.com>
2024-06-30 14:25:53 +0530

    completed hero-secton 1

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import HeroCarousel from "@/components/HeroCarousel"
+import Searchbar from "@/components/Searchbar"
 import Image from "next/image"
 
 const Home = () => {
   return (
     <>
-    <section className="px-6 md:px-20 py-24 border-2 border-red-500 ">
+    <section className="px-6 md:px-20 py-24 ">
         <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center">
             <p className="small-text">
@@ -23,9 +25,9 @@ const Home = () => {
               <p className="mt-6">
                 Powerful, self-serve product and growth analytics to help you convert,engage,and retain more.
               </p>
-              Searchbar
+              <Searchbar/>
           </div>
-          HeroCarousel
+          <HeroCarousel/>
         </div>
     </section>
     <section className="trending-section">
---
+++ components/Searchbar.tsx
@@ -0,0 +1,45 @@
+"use client"
+import React, { FormEvent, useState } from 'react'
+
+const Searchbar = () => {
+    const [searchPrompt,setSearchPrompt]=useState("");
+    const [isLoading,setIsLoading]=useState(false);
+    const isValidAmazonProductURL=(url:string)=>{
+        try{
+            const parsedURL=new URL(url);
+            const hostName=parsedURL.hostname;
+            if(hostName.includes('amazon.com') || hostName.includes('amazon.') || hostName.endsWith('amazon')){
+                return true;
+            }
+        }catch(e){
+            return false;
+        }
+        return false;
+    }
+    const handleSubmit = (event:FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const isValidLink=isValidAmazonProductURL(searchPrompt);
+        if(!isValidLink) return alert ("Please provide a valid Amazon link")
+
+        try{
+            setIsLoading(true);
+            // scrape the first product
+        }catch(e){
+            console.log(e)
+        }finally{
+            setIsLoading(false);
+        }
+    }
+  return (
+    <form className='flex flex-wrap gap-4 mt-12' onSubmit={handleSubmit}>
+        <input type="text"
+        value={searchPrompt}
+        onChange={(e)=>setSearchPrompt(e.target.value)}
+        placeholder='Enter product link'
+        className='searchbar-input' />
+        <button type='submit' disabled={searchPrompt===""} className='searchbar-btn'>{isLoading? "Searching...": "Search"}</button>
+    </form>
+  )
+}
+
+export default Searchbar

a4791c026c4b38c97e475111413870a15f75a35b
abhay gupta <akg568794@gmail.com>
2024-07-01 08:20:45 +0530

    ( 1:48:33 )

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -6,7 +6,11 @@ export async function scrapeAndStoreProduct(productUrl:string){
     if(!productUrl) return;
     try{
         const scrapedProduct=await scrapeAmazonProduct(productUrl);
+        if(!scrapedProduct) return;
+
+        
     }catch(error:any){
         throw new Error(`Failed to create/update product: ${error.message}`)
     }
 }
+
--- lib/scraper/index.ts
+++ lib/scraper/index.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import * as cheerio from "cheerio";
-import { extractPrice } from "../utils";
+import { extractCurrency, extractDescription, extractPrice } from "../utils";
+
 
 export async function scrapeAmazonProduct(url:string){
     if(!url) return;
@@ -30,9 +31,47 @@ try{
         $('a.size.base.a-color-price'),
         $('.a-button-selected .a-color-base'),
         $('.a-price.a-text-price')
-
     );
-    console.log({currentPrice,title})
+
+    const originalPrice = extractPrice(
+        $('#priceblock_ourprice'),
+        $('.a-price.a-text-price span.a-offscreen'),
+        $('#listPrice'),
+        $('#priceblock_dealprice'),
+        $('.a-size-base.a-color-price')
+      );
+
+    const outOfStock=$('#availability span').text().trim().toLowerCase()==='currently unavailable';
+
+    const images=$('#imgBlkFront').attr('data-a-dynamic-image') || $('#landingImage').attr('data-a-dynamic-image') || '{}'
+    const imageUrls=Object.keys(JSON.parse(images))
+    const currency=extractCurrency($('.a-price-symbol'))
+
+    const discountRate=$('.savingsPercentage').text().replace(/[-%]/g," ");
+
+    const description=extractDescription($);
+
+    // construct data object with scraped information
+    const data={
+        url,
+        currency:currency || 'USD',
+        image: imageUrls[0],
+        title,
+        currentPrice:Number(currentPrice) || Number(currentPrice),
+        originalPrice:Number(originalPrice) || Number(originalPrice),
+        priceHistory:[],
+        discountRate:Number(discountRate),
+        category:'category',
+        reviewsCount:100,
+        stars:4.5,
+        isOutOfStock:outOfStock,
+        description,
+        lowestPrice:Number(currentPrice) || Number(originalPrice),
+        highestPrice:Number(originalPrice) || Number(currentPrice),
+        average:Number(currentPrice) || Number(originalPrice)
+    }
+
+    return data;
 }catch(err:any){
     throw new Error(`falied to scrape product: ${err.message}`);
 

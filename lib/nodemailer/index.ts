4824e7d03aa392241a821748aaf42993e92403b9
abhay gupta <akg568794@gmail.com>
2024-07-03 23:42:26 +0530

    Created cron job

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -81,19 +81,19 @@ export async function generateEmailBody(
 }
 
 const transporter = nodemailer.createTransport({
-    pool: true,
-    service: 'hotmail',
-    port: 2525,
-    auth: {
-      user: 'akg568794@gmail.com',
-      pass: process.env.EMAIL_PASSWORD,
-    },
-    maxConnections: 1
-  })
+  pool: true,
+  service: 'hotmail',
+  port: 2525,
+  auth: {
+    user: 'akg568794@gmail.com',
+    pass: process.env.EMAIL_PASSWORD,
+  },
+  maxConnections: 1
+})
 
 export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) => {
   const mailOptions = {
-    from: '',
+    from: 'akg568794@gmail.com',
     to: sendTo,
     html: emailContent.body,
     subject: emailContent.subject,
@@ -101,5 +101,7 @@ export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) =>
 
   transporter.sendMail(mailOptions, (error: any, info: any) => {
     if(error) return console.log(error);
+    
+    console.log('Email sent: ', info);
   })
 }

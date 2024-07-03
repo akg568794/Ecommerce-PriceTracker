ea2ef6ffb9252c9254e81e2d806fedd49da95f13
abhay gupta <akg568794@gmail.com>
2024-07-03 20:30:55 +0530

    make some changes in the nodemailer

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -101,7 +101,5 @@ export const sendEmail = async (emailContent: EmailContent, sendTo: string[]) =>
 
   transporter.sendMail(mailOptions, (error: any, info: any) => {
     if(error) return console.log(error);
-    
-    console.log('Email sent: ', info);
   })
 }
